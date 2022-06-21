e37b63e3b34117fe3e8f10dc96b374d0f8b0a813
Restu Edo Setiaji <restuedosetiaji@gmail.com>
2022-06-21 16:41:04 +0700

    Update 2022_04_18_040951_create_portfolios_table.php

--- database/migrations/2022_04_18_040951_create_portfolios_table.php
+++ database/migrations/2022_04_18_040951_create_portfolios_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePortfolioTable extends Migration
+class CreatePortfoliosTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreatePortfolioTable extends Migration
      */
     public function up()
     {
-        Schema::create('portfolio', function (Blueprint $table) {
+        Schema::create('portfolios', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
         });
@@ -26,6 +26,6 @@ class CreatePortfolioTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('portfolio');
+        Schema::dropIfExists('portfolios');
     }
 }
