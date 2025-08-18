a89266fc6f66ab24fdd7a98901741b9b712b1e5b
Restu Edo Setiaji <restuedosetiaji@gmail.com>
2025-08-18 13:44:44 +0700

    feat: install filament v4

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,24 @@
 import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import laravel, { refreshPaths }  from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
+            refresh: [
+                ...refreshPaths,
+                'app/Livewire/**',
+            ],
         }),
         tailwindcss(),
     ],
+    server: {
+        cors: true,
+        host: '0.0.0.0',
+        port: 5173,
+        hmr: {
+            host: process.env.VITE_HMR_HOST || 'localhost',
+        },
+    },
 });
