0100026df149f646caa41902f4dd5de9dc026a3e
Restu Edo Setiaji <restuedosetiaji@gmail.com>
2025-08-20 14:30:19 +0700

    feat: add telescope

--- app/Providers/Filament/ClientPanelProvider.php
+++ app/Providers/Filament/ClientPanelProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers\Filament;
 
 use Althinect\FilamentSpatieRolesPermissions\FilamentSpatieRolesPermissionsPlugin;
+use Filament\Actions\Action;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\AuthenticateSession;
 use Filament\Http\Middleware\DisableBladeIconComponents;
@@ -35,6 +36,14 @@ class ClientPanelProvider extends PanelProvider
             ->profile(isSimple: false)
             ->colors([
                 'primary' => Color::Red,
+            ])
+            ->userMenuItems([
+                'telescope' => Action::make('telescope')
+                    ->label('Telescope')
+                    ->icon('heroicon-o-bug-ant')
+                    ->url(fn () => config('telescope.enabled') ? route('telescope') : null)
+                    ->visible(fn () => config('telescope.enabled') && auth()->user()?->can('viewTelescope'))
+                    ->openUrlInNewTab(),
             ])
             ->discoverResources(in: app_path('Filament/Client/Resources'), for: 'App\Filament\Client\Resources')
             ->discoverPages(in: app_path('Filament/Client/Pages'), for: 'App\Filament\Client\Pages')
